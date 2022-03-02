9d6c0dc958457c5839c841552b5e3e431c93c5aa
PengMingJiu <pengmingjiu@gmail.com>
2022-03-02 15:16:04 +0800

    fix:patchVModel 致命错误

--- examples/UserFormatter.vue
+++ examples/UserFormatter.vue
@@ -1,22 +1,31 @@
 <template>
   <div style="height: 97vh">
     <crud-table>
-      <crud-table-header inline>
-        <crud-table-search :formatter="searchFormatter">
-          <el-form-item prop="name">
-            <el-input placeholder="姓名"/>
-          </el-form-item>
-          <el-form-item prop="age">
-            <el-input placeholder="年龄"/>
-          </el-form-item>
-        </crud-table-search>
-      </crud-table-header>
       <crud-table-pagination>
         <crud-table-data url="http://localhost:5000/list">
           <el-table-column prop="id" label="id"></el-table-column>
           <el-table-column prop="name" label="姓名"></el-table-column>
+          <crud-table-handler>
+            <crud-table-btn-edit url="http://localhost:5000/edit"/>
+          </crud-table-handler>
         </crud-table-data>
       </crud-table-pagination>
+      <crud-table-dialog>
+        <div>
+          <el-row>
+            <el-col :span="24">
+              <el-form-item label="姓名" prop="name">
+                <el-input placeholder="姓名"/>
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="年龄" prop="age">
+                <el-input placeholder="年龄"/>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </div>
+      </crud-table-dialog>
     </crud-table>
   </div>
 </template>
--- src/core/patchVModel.js
+++ src/core/patchVModel.js
@@ -5,27 +5,31 @@ export default function patchVModel(vNodes, obj, isReadonly = false) {
   if (!vNodes) return null
   return vNodes.map(node => {
     const nodeType = node.type
-    if (type(nodeType) !== 'object') return null
-    if (nodeType.name !== "ElFormItem") {
-      return patchVModel(node.children, obj, isReadonly)
-    }
-    const {prop, skip} = node.props || {}
-    if (skip !== undefined) return node
-    if (!prop) console.error("请给 ElFormItem 设置 prop 属性")
-    const childrenNode = node.children['default']()
-    return h(node, () => childrenNode.map(child => {
-      if (isReadonly) {
+    if (nodeType.name === "ElFormItem") {
+      const {prop, skip} = node.props || {}
+      if (skip !== undefined) return node
+      if (!prop) console.error("请给 ElFormItem 设置 prop 属性")
+      const childrenNode = node.children['default']()
+      return h(node, () => childrenNode.map(child => {
+        if (isReadonly) {
+          return h(child, {
+            modelValue: obj[prop],
+            readonly: true
+          })
+        }
         return h(child, {
           modelValue: obj[prop],
-          readonly: true
+          'onUpdate:modelValue': value => {
+            obj[prop] = value
+          }
         })
-      }
-      return h(child, {
-        modelValue: obj[prop],
-        'onUpdate:modelValue': value => {
-          obj[prop] = value
-        }
-      })
-    }))
+      }))
+    }
+    if (type(nodeType) === 'string') {
+      //普通元素
+      if (type(node.children) === 'string') return node
+      return h(node, patchVModel(node.children, obj, isReadonly))
+    }
+    return h(node, () => patchVModel(node.children.default(), obj, isReadonly))
   })
 }
